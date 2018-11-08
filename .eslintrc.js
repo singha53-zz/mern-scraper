72aa3d78f626397b287accb53b946b9338516c0d
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-11-08 14:57:45 -0500

    update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-    "extends": [ "airbnb", "plugin:react/recommended" ],
-    "rules": {
-        'jsx-a11y/label-has-associated-control': [
-      2,
-      {
-        labelComponents: ['label'],
-      },
-    ],
-    }
+    "extends": [ "airbnb", "plugin:react/recommended" ]
 };
